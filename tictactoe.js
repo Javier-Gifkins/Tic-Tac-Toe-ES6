315dadd1ae13cdd7d957c57adb572bcf37a09f71
jav <javier-gifkins@live.nmit.ac.nz>
2025-05-10 13:15:54 +1200

    Step 1.1: small fix to game board

--- tictactoe.js
+++ tictactoe.js
@@ -1,7 +1,7 @@
 //NOTE - Step 1: Setting up game board
 
 // initialising a 3x3 game board (empty spots are represented by numbers 1 through 9)
-let board = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
+let board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
 
 // the board is represented as an array with numbers 1 through 9. each number corresponds to a position on the grid.
 
@@ -26,3 +26,5 @@ function printBoard() {
 
 // print example board
 printBoard()
+
+//NOTE - Step 2: Programming player move
